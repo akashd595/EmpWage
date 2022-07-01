cdda2aa1364dd864ab3d59fb06e4f4be397380b9
Akash Das <akashd595@gmail.com>
2022-07-01 23:01:15 +0530

    ADDED: EmpWage

---
+++ EmpWage.java
@@ -0,0 +1,9 @@
+package com.EmpWage;
+
+public class EmpWage {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+    }
+}
