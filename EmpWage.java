1b0e6341d1cb7f4709922d7b6bfdfe2508c3474f
Akash Das <akashd595@gmail.com>
2022-07-01 23:08:23 +0530

    ADDED: Calculating Daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -2,7 +2,9 @@ package com.EmpWage;
 import java.util.Random;
 
 public class EmpWage {
-    static int IS_FULL_DAY = 1;
+    private static final int ABSENT_DAY_HOUR = 0;
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOUR = 8;
     static int IS_ABSENT_TODAY = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
@@ -10,11 +12,14 @@ public class EmpWage {
         Random random = new Random();
         int attendance = random.nextInt(2);
 
-
+        int total_empl_wage=0;
         if (attendance == IS_ABSENT_TODAY) {
             System.out.println("Employee is absent today");
+            total_empl_wage += WAGE_PER_HOUR*ABSENT_DAY_HOUR;
         } else {
             System.out.println("Employee is Present today");
+            total_empl_wage += WAGE_PER_HOUR*FULL_DAY_HOUR;
         }
+        System.out.println("Total wage:- "+total_empl_wage);
     }
 }
